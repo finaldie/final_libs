2e981f6ed8c57c01dedcb68fbf7edbad03e1f13d
huyuzhang <hyzwowtools@gmail.com>
2011-11-27 11:08:52 +0800

    add test case of test mbuf_pop

--- test/t_mbuf.c
+++ test/t_mbuf.c
@@ -323,7 +323,23 @@ void test_mbuf1()
     }
 
     {
+        int ret = mbuf_pop(pbuf, pop_buf, 3);
+        FTU_ASSERT_EQUAL_INT(0, ret);
 
+        int size = mbuf_size(pbuf);
+        FTU_ASSERT_EQUAL_INT(10, size);
+        
+        int used = mbuf_used(pbuf);
+        FTU_ASSERT_EQUAL_INT(1, used);
+        
+        int head_free = mbuf_head_free(pbuf);
+        FTU_ASSERT_EQUAL_INT(4, head_free);
+
+        int tail_free = mbuf_tail_free(pbuf);
+        FTU_ASSERT_EQUAL_INT(5, tail_free);
+
+        int total_free = mbuf_free(pbuf);
+        FTU_ASSERT_EQUAL_INT(8, total_free);
     }
 
     mbuf_delete(pbuf);
