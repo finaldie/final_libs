a3013cd53cfb6400c38181234b7ec435020c5409
huyuzhang <hyzwowtools@gmail.com>
2012-07-20 00:05:04 +0800

    re-format fconf files

--- fconf/conf.c
+++ fconf/conf.c
@@ -7,114 +7,106 @@
 #include <sys/stat.h>
 #include "conf.h"
 
-//return	0:normal 1:error
-int		ReadConfig(char* filename, char* pBuf, unsigned int len)
+//return    0:normal 1:error
+int     ReadConfig(char* filename, char* pBuf, unsigned int len)
 {
-	int fd, bytes_read = 0;
+    int fd, bytes_read = 0;
 
-	if( (fd = open(filename, O_RDONLY)) == -1 )
-	{
-		printf("open file error\n");
-		return -1;
-	}
+    if ( (fd = open(filename, O_RDONLY)) == -1 ) {
+        printf("open file error\n");
+        return -1;
+    }
 
-	bytes_read = read(fd, pBuf, len);  
-		
-	if( (bytes_read == -1) ) 
-		return	-1; 
-	else if( bytes_read > 0 ) 
-	{
-		pBuf[bytes_read] = '\0';
-	}
+    bytes_read = read(fd, pBuf, len);
+    if ( (bytes_read == -1) )
+        return    -1; 
+    else if ( bytes_read > 0 ) {
+        pBuf[bytes_read] = '\0';
+    }
 
-	return	bytes_read;
+    return bytes_read;
 }
 
-int	 	IsToken(char pSrc, char pCmp)
+int     IsToken(char pSrc, char pCmp)
 {
-	if( pSrc == pCmp )
-		return 1;
-	return 0;
+    if ( pSrc == pCmp )
+        return 1;
+    return 0;
 }
 
-int 	IsTokenEnd( char pStr )
+int     IsTokenEnd( char pStr )
 {
-	return	IsToken(pStr, '\0');
+    return IsToken(pStr, '\0');
 }
 
-int		IsTokenEnter( char pStr )
+int     IsTokenEnter( char pStr )
 {
-	return	IsToken(pStr, '\n');
+    return IsToken(pStr, '\n');
 }
 
-int		IsTokenNotes( char pStr )
+int     IsTokenNotes( char pStr )
 {
-	return	IsToken(pStr, '#');
+    return IsToken(pStr, '#');
 }
 
-int	 	IsTokenBlank( char pStr )
+int     IsTokenBlank( char pStr )
 {
-	return IsToken(pStr, ' ');
+    return IsToken(pStr, ' ');
 }
 
-int		ReadLine(char* pBufSrc, unsigned int Start, char* pLine)
+int     ReadLine(char* pBufSrc, unsigned int Start, char* pLine)
 {
-	int len = 0;
+    int len = 0;
 
-	while( !IsTokenEnd(pBufSrc[Start]) )
-		if( IsTokenBlank(pBufSrc[Start]) )
-			Start++;
-		else
-			break;
+    while ( !IsTokenEnd(pBufSrc[Start]) )
+        if ( IsTokenBlank(pBufSrc[Start]) )
+            Start++;
+        else
+            break;
 
-	if( IsTokenEnd(pBufSrc[Start]) )
-		return -1;
+    if ( IsTokenEnd(pBufSrc[Start]) )
+        return -1;
 
-	while( !IsTokenEnd(pBufSrc[Start]) && !IsTokenEnter(pBufSrc[Start]) )
-	{
-		if( !IsTokenNotes(pBufSrc[Start]) )
-		{
-			pLine[len++] = pBufSrc[Start++];
-		}
-		else
-		{
-			break;
-		}
-	}
-	pLine[len] = '\0';
+    while ( !IsTokenEnd(pBufSrc[Start]) && !IsTokenEnter(pBufSrc[Start]) ) {
+        if ( !IsTokenNotes(pBufSrc[Start]) ) {
+            pLine[len++] = pBufSrc[Start++];
+        } else {
+            break;
+        }
+    }
+    pLine[len] = '\0';
 
-	while( !IsTokenEnd(pBufSrc[Start]) && !IsTokenEnter(pBufSrc[Start]) )
-		Start++;
+    while ( !IsTokenEnd(pBufSrc[Start]) && !IsTokenEnter(pBufSrc[Start]) )
+        Start++;
 
-	return ++Start;
+    return ++Start;
 }
 
-int		ReadWord(char* pLine, char* pWord, int lp)
+int     ReadWord(char* pLine, char* pWord, int lp)
 {
-	int lp_word = 0;
-	int	lenofline = strlen(pLine);
+    int lp_word = 0;
+    int lenofline = strlen(pLine);
 
-	if( lenofline == 0 )
-		return -1;
-	
-	while( !IsTokenEnd(pLine[lp]) )
-		if( IsTokenBlank(pLine[lp]) )
-			lp++;
-		else
-			break;
+    if ( lenofline == 0 )
+        return -1;
 
-	if( IsTokenEnd(pLine[lp]) )
-		return -1;
+    while ( !IsTokenEnd(pLine[lp]) )
+        if ( IsTokenBlank(pLine[lp]) )
+            lp++;
+        else
+            break;
 
-	while( !IsTokenEnd(pLine[lp]) )
-	{
-		if( !IsTokenBlank(pLine[lp]) )
-			pWord[lp_word++] = pLine[lp++];
-		else
-			break;
-	}
+    if ( IsTokenEnd(pLine[lp]) )
+        return -1;
 
-	pWord[lp_word] = '\0';
+    while ( !IsTokenEnd(pLine[lp]) ) {
+        if ( !IsTokenBlank(pLine[lp]) )
+            pWord[lp_word++] = pLine[lp++];
+        else
+            break;
+    }
 
-	return	lp;
+    pWord[lp_word] = '\0';
+
+    return lp;
 }
--- fconf/conf.h
+++ fconf/conf.h
@@ -1,16 +1,16 @@
-// note: the '#' in the content means note line
-
-#ifndef	_CONF_H_FINAL_
+#ifndef _CONF_H_FINAL_
 #define _CONF_H_FINAL_
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int		ReadConfig(char* filename, char* pBuf, unsigned int len);
-int		ReadLine(char* pBufSrc, unsigned int Start, char* pLine);
-int		ReadWord(char* pLine, char* pWord, int lp);
-int	 	IsToken(char pSrc, char pCmp);
+// note: the '#' in the content means note line
+
+int ReadConfig(char* filename, char* pBuf, unsigned int len);
+int ReadLine(char* pBufSrc, unsigned int Start, char* pLine);
+int ReadWord(char* pLine, char* pWord, int lp);
+int IsToken(char pSrc, char pCmp);
 
 #ifdef __cplusplus
 }
