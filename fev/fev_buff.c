9b5845d7f76ab1994a1fcfa37f6a5d67e644e884
huyuzhang <hyzwowtools@gmail.com>
2012-07-20 01:12:46 +0800

    re-format fev files

--- fev/fev_buff.c
+++ fev/fev_buff.c
@@ -10,8 +10,7 @@
  *       Revision:  none
  *       Compiler:  gcc
  *
- *         Author:  YOUR NAME (), 
- *        Company:  
+ *         Author:  yuzhang hu
  *
  * =====================================================================================
  */
@@ -36,57 +35,58 @@ struct fev_buff {
     void*           arg;
 };
 
-static int fev_read(int fd, void* pbuf, size_t len)
+static
+int fev_read(int fd, void* pbuf, size_t len)
 {
-    do{
-		int read_size = read(fd, pbuf, len);
+    do {
+        int read_size = read(fd, pbuf, len);
 
-		if ( read_size == -1 )
-		{
-			if( errno == EINTR )
-				continue;
-			else if( errno == EAGAIN )
-				return 0;   // we can ignore this and simply to return 0
-			return -1;
-		}
-		else if( read_size == 0 )
-			return -1;
-		else
-			return read_size;
-	}while(1);
+        if ( read_size == -1 ) {
+            if( errno == EINTR )
+                continue;
+            else if( errno == EAGAIN )
+                return 0;   // we can ignore this and simply to return 0
+            return -1;
+        } else if( read_size == 0 )
+            return -1;
+        else
+            return read_size;
+    } while (1);
 }
 
-static int fev_write(int fd, const char* pbuf, size_t len)
+static
+int fev_write(int fd, const char* pbuf, size_t len)
 {
-    do{
-		int bytes = write(fd, pbuf, len);
+    do {
+        int bytes = write(fd, pbuf, len);
 
-		if( bytes > 0 )
-			return bytes;
-		else
-		{
-			if( errno == EINTR )
-				continue;
-			else if( errno == EAGAIN )
-				return 0;
-			else
-				return -1;
-		}
-	}while(1);
+        if ( bytes > 0 )
+            return bytes;
+        else {
+            if( errno == EINTR )
+                continue;
+            else if( errno == EAGAIN )
+                return 0;
+            else
+                return -1;
+        }
+    } while (1);
 }
 
-static void evbuff_read(fev_state* fev, int fd, int mask, void* arg)
+static
+void evbuff_read(fev_state* fev, int fd, int mask, void* arg)
 {
     fev_buff* evbuff = (fev_buff*)arg;
-    if( evbuff->read_cb ) 
+    if ( evbuff->read_cb )
         evbuff->read_cb(fev, evbuff, evbuff->arg);
 }
 
-static void evbuff_write(fev_state* fev, int fd, int mask, void* arg)
+static
+void evbuff_write(fev_state* fev, int fd, int mask, void* arg)
 {
     fev_buff* evbuff = (fev_buff*)arg;
 
-    do{
+    do {
         int buf_len = mbuf_used(evbuff->wbuf);
         if ( buf_len == 0 ) {
             fev_del_event(evbuff->fstate, evbuff->fd, FEV_WRITE);
@@ -94,32 +94,30 @@ static void evbuff_write(fev_state* fev, int fd, int mask, void* arg)
         }
 
         int bytes = fev_write(evbuff->fd, mbuf_get_head(evbuff->wbuf), buf_len);
-        if( bytes > 0 ) {
+        if ( bytes > 0 ) {
             mbuf_head_seek(evbuff->wbuf, bytes);
-        }
-        else if( bytes == 0 ) { // EAGAIN
+        } else if ( bytes == 0 ) { // EAGAIN
             break;
-        }  
-        else {
-            if ( evbuff->error_cb ) 
+        } else {
+            if ( evbuff->error_cb )
                 evbuff->error_cb(fev, evbuff, evbuff->arg);
             return;
         }
 
-    }while(1);
+    } while (1);
 }
 
-fev_buff*	fevbuff_new(
+fev_buff*    fevbuff_new(
                 fev_state* fev,
                 int fd,
-				fev_buff_read read_cb,		// call when the fd can read
-				fev_buff_error error_cb,	// call when socket has error
-				void* arg)                  // user argument
+                fev_buff_read read_cb,        // call when the fd can read
+                fev_buff_error error_cb,    // call when socket has error
+                void* arg)                  // user argument
 {
-    if( !fev ) return NULL;
+    if ( !fev ) return NULL;
 
-    fev_buff* evbuff = (fev_buff*)malloc(sizeof(fev_buff));
-    if( !evbuff ) return NULL;
+    fev_buff* evbuff = malloc(sizeof(fev_buff));
+    if ( !evbuff ) return NULL;
 
     evbuff->fstate = fev;
     evbuff->fd = fd;
@@ -128,13 +126,13 @@ fev_buff*	fevbuff_new(
     evbuff->error_cb = error_cb;
 
     evbuff->rbuf = mbuf_create(FEV_BUFF_DEFAULT_SIZE);
-    if( !evbuff->rbuf ) {
+    if ( !evbuff->rbuf ) {
         free(evbuff);
         return NULL;
     }
 
     evbuff->wbuf = mbuf_create(FEV_BUFF_DEFAULT_SIZE);
-    if( !evbuff->wbuf ) {
+    if ( !evbuff->wbuf ) {
         mbuf_delete(evbuff->rbuf);
         free(evbuff);
         return NULL;
@@ -150,9 +148,9 @@ fev_buff*	fevbuff_new(
     return evbuff;
 }
 
-int  fevbuff_destroy(fev_buff* evbuff)
+int     fevbuff_destroy(fev_buff* evbuff)
 {
-    if( !evbuff ) return -1;
+    if ( !evbuff ) return -1;
 
     int fd = evbuff->fd;
     int mask = FEV_READ | FEV_WRITE;
@@ -165,7 +163,7 @@ int  fevbuff_destroy(fev_buff* evbuff)
     return fd;
 }
 
-int fevbuff_get_fd(fev_buff* evbuff)
+int     fevbuff_get_fd(fev_buff* evbuff)
 {
     return evbuff->fd;
 }
@@ -175,17 +173,17 @@ void*   fevbuff_get_arg(fev_buff* evbuff)
     return evbuff->arg;
 }
 
-int fevbuff_get_bufflen(fev_buff* evbuff, int type)
+int     fevbuff_get_bufflen(fev_buff* evbuff, int type)
 {
-    if( type == FEVBUFF_TYPE_READ )
+    if ( type == FEVBUFF_TYPE_READ )
         return mbuf_size(evbuff->rbuf);
     else
         return mbuf_size(evbuff->wbuf);
 }
 
-int fevbuff_get_usedlen(fev_buff* evbuff, int type)
+int     fevbuff_get_usedlen(fev_buff* evbuff, int type)
 {
-    if( type == FEVBUFF_TYPE_READ )
+    if ( type == FEVBUFF_TYPE_READ )
         return mbuf_used(evbuff->rbuf);
     else
         return mbuf_used(evbuff->wbuf);
@@ -193,12 +191,12 @@ int fevbuff_get_usedlen(fev_buff* evbuff, int type)
 
 // if pbuf != NULL, return data_len and copy data to user
 // if pbuf == NULL, return data_len without copy data
-int fevbuff_read(fev_buff* evbuff, void* pbuf, size_t len)
+int     fevbuff_read(fev_buff* evbuff, void* pbuf, size_t len)
 {
     int used_len = fevbuff_get_usedlen(evbuff, FEVBUFF_TYPE_READ);
     int need_read_bytes = len - used_len;
 
-    if( need_read_bytes <= 0 ) {
+    if ( need_read_bytes <= 0 ) {
         if( pbuf ) memcpy(pbuf, mbuf_get_head(evbuff->rbuf), len);
         return (int)len;
     }
@@ -219,8 +217,7 @@ int fevbuff_read(fev_buff* evbuff, void* pbuf, size_t len)
         int copy_bytes = used > len ? len : used;
         if( pbuf ) memcpy(pbuf, mbuf_get_head(evbuff->rbuf), copy_bytes);
         return (int)copy_bytes;
-    }
-    else {
+    } else {
         //error
         if( evbuff->error_cb ) 
             evbuff->error_cb(evbuff->fstate, evbuff, evbuff->arg);
@@ -228,16 +225,17 @@ int fevbuff_read(fev_buff* evbuff, void* pbuf, size_t len)
     }
 }
 
-static int fevbuff_cache_data(fev_buff* evbuff, const void* pbuf, size_t len)
+static
+int     fevbuff_cache_data(fev_buff* evbuff, const void* pbuf, size_t len)
 {
-    if( len == 0 ) return 0;
+    if ( len == 0 ) return 0;
 
     int tail_free = mbuf_tail_free(evbuff->wbuf);
-    if( tail_free < (int)len ){
+    if ( tail_free < (int)len ) {
         mbuf_rewind(evbuff->wbuf);
 
         tail_free = mbuf_tail_free(evbuff->wbuf);
-        if( tail_free < (int)len ){
+        if ( tail_free < (int)len ) {
             evbuff->wbuf = mbuf_realloc(evbuff->wbuf, len + mbuf_used(evbuff->wbuf));
         }
     }
@@ -248,25 +246,25 @@ static int fevbuff_cache_data(fev_buff* evbuff, const void* pbuf, size_t len)
     return 0;
 }
 
-int	fevbuff_write(fev_buff* evbuff, const void* pbuf, size_t len)
+int    fevbuff_write(fev_buff* evbuff, const void* pbuf, size_t len)
 {
-    if( !pbuf ) return 0;
-    if( len == 0 ) return 0;
+    if ( !pbuf ) return 0;
+    if ( len == 0 ) return 0;
 
     int bytes = 0;
     mbuf* wbuf = evbuff->wbuf;
-	int write_buf_len = mbuf_used(wbuf);
-    if( write_buf_len == 0 ){
+    int write_buf_len = mbuf_used(wbuf);
+    if ( write_buf_len == 0 ) {
         // send immediately
         bytes = fev_write(evbuff->fd, pbuf, len);
-        if( bytes < 0 ){
-            if(evbuff->error_cb) 
+        if ( bytes < 0 ) {
+            if (evbuff->error_cb) 
                 evbuff->error_cb(evbuff->fstate, evbuff, evbuff->arg);
             return -1;
         }
     }
-    
-    if( len - bytes > 0 ) {
+
+    if ( len - bytes > 0 ) {
         // cache data
         fevbuff_cache_data(evbuff, pbuf+bytes, len-bytes);
     }
@@ -275,9 +273,9 @@ int	fevbuff_write(fev_buff* evbuff, const void* pbuf, size_t len)
 }
 
 // this interface only operate read buf casue write buf is not visible for user
-int fevbuff_pop(fev_buff* evbuff, size_t len)
+int     fevbuff_pop(fev_buff* evbuff, size_t len)
 {
-    if( len == 0 ) return 0;
+    if ( len == 0 ) return 0;
 
     int buf_len = mbuf_used(evbuff->rbuf);
     int pop_len = buf_len > (int)len ? (int)len : buf_len; 
@@ -289,7 +287,7 @@ int fevbuff_pop(fev_buff* evbuff, size_t len)
 // return readbuff head pointer
 void*   fevbuff_rawget(fev_buff* evbuff)
 {
-    if( !evbuff ) return NULL;
+    if ( !evbuff ) return NULL;
 
     return mbuf_get_head(evbuff->rbuf);
 }
--- fev/fev_conn.c
+++ fev/fev_conn.c
@@ -10,8 +10,7 @@
  *       Revision:  none
  *       Compiler:  gcc
  *
- *         Author:  finaldie
- *        Company:  
+ *         Author:  yuzhang hu(finaldie)
  *
  * =====================================================================================
  */
@@ -31,44 +30,46 @@ typedef struct fev_conn_info {
     fev_timer*  timer;
     pfev_conn   conn_cb;
     conn_arg_t  arg;
-}fev_conn_info;
+} fev_conn_info;
 
-static void on_connect(fev_state* fev, int fd, int mask, void* arg)
+static
+void    on_connect(fev_state* fev, int fd, int mask, void* arg)
 {
     fev_conn_info* conn_info = (fev_conn_info*)arg;
     fev_del_event(fev, conn_info->fd, FEV_READ | FEV_WRITE);
     fev_del_timer_event(fev, conn_info->timer);
 
-    if( mask & FEV_ERROR ) {
+    if ( mask & FEV_ERROR ) {
         goto CONN_ERROR;
     }
-    
+
     int err = 0;
     socklen_t len = sizeof(int);
-    if (( 0 == getsockopt(conn_info->fd, SOL_SOCKET, SO_ERROR, &err, &len) )){
-        if( 0 == err ) {
-            if( conn_info->conn_cb )
+    if (( 0 == getsockopt(conn_info->fd, SOL_SOCKET, SO_ERROR, &err, &len) )) {
+        if ( 0 == err ) {
+            if ( conn_info->conn_cb )
                 conn_info->conn_cb(conn_info->fd, conn_info->arg);
             goto CONN_END;
         }
     }
-    
+
 CONN_ERROR:
     close(conn_info->fd);
 
-    if( conn_info->conn_cb )
+    if ( conn_info->conn_cb )
         conn_info->conn_cb(-1, conn_info->arg);
 CONN_END:
     free(conn_info);
 }
 
-static void on_timer(fev_state* fev, void* arg)
+static
+void    on_timer(fev_state* fev, void* arg)
 {
     fev_conn_info* conn_info = (fev_conn_info*)arg;
     fev_del_event(fev, conn_info->fd, FEV_READ | FEV_WRITE);
     close(conn_info->fd);
 
-    if( conn_info->conn_cb )
+    if ( conn_info->conn_cb )
         conn_info->conn_cb(-1, conn_info->arg);
 
     free(conn_info);
@@ -82,44 +83,42 @@ int    fev_conn(fev_state* fev,
             conn_arg_t arg)
 {
     int sockfd = -1;
-	int s = net_conn_a(ip, port, &sockfd);
+    int s = net_conn_a(ip, port, &sockfd);
 
-    if( !pfunc ) abort();
+    if ( !pfunc ) abort();
 
-	if( s == 0 ){	// connect sucess
+    if ( s == 0 ){    // connect sucess
         if ( pfunc ) pfunc(sockfd, arg);
         return 0;
-	}
-	else if( s == -1 ){ // connect error
+    } else if ( s == -1 ){ // connect error
         return -1;
-	}
-	else{
-        fev_conn_info* conn_info = (fev_conn_info*)malloc(sizeof(fev_conn_info));
-        if( !conn_info ){
+    } else {
+        fev_conn_info* conn_info = malloc(sizeof(fev_conn_info));
+        if ( !conn_info ) {
             close(sockfd);
             return -1;
         }
 
         conn_info->fd = sockfd;
-        conn_info->timer = fev_add_timer_event(fev, (long)timeout * 1000000l, 0, on_timer, conn_info);
-        if( !conn_info->timer ) {
+        conn_info->timer = fev_add_timer_event(fev, (long)timeout * 1000000l,
+                                                0, on_timer, conn_info);
+        if ( !conn_info->timer ) {
             printf("fev_conn init timer failed sockfd=%d\n", sockfd);
             close(sockfd);
             free(conn_info);
-
             return -1;
         }
 
         conn_info->conn_cb = pfunc;
         conn_info->arg = arg;
 
-        int ret = fev_reg_event(fev, sockfd, FEV_WRITE, NULL, on_connect, conn_info);
-        if ( ret != 0 ){
+        int ret = fev_reg_event(fev, sockfd, FEV_WRITE, NULL,
+                                on_connect, conn_info);
+        if ( ret != 0 ) {
             printf("fev_conn reg_event failed sockfd=%d ret=%d\n", sockfd, ret);
             fev_del_timer_event(fev, conn_info->timer);
             close(sockfd);
             free(conn_info);
-
             return -1;
         }
 
