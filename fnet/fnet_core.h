6bcf27d1334e83ccdb95b513ded57c4c943bd880
huyuzhang <hyzwowtools@gmail.com>
2013-11-17 21:01:04 -0500

    add define SO_REUSEPORT to enable the feature for those doesn't upgrade libc6-dev to latest users

--- fnet/fnet_core.h
+++ fnet/fnet_core.h
@@ -11,6 +11,11 @@ extern "C" {
 #define SOCKET_ERROR    -1
 #define SOCKET_CLOSE    0
 
+// This feature enabled when kernel >= 3.9
+#ifndef SO_REUSEPORT
+#define SO_REUSEPORT 15
+#endif
+
 // host struct
 typedef struct {
     int    alias_count;
