1f7ee3e17ded77c83d4029d9474896b1f1e227b8
Yuzhang Hu <hyzwowtools@gmail.com>
2014-05-03 06:57:55 +0000

    fhash refactor

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -41,11 +41,10 @@ flags = [
 '-Wc++98-compat',
 '-Wno-long-long',
 '-Wno-variadic-macros',
+'-Wno-unused-parameter',
+'-Wpadded',
 '-fexceptions',
 '-DNDEBUG',
-# You 100% do NOT need -DUSE_CLANG_COMPLETER in your flags; only the YCM
-# source code needs it.
-#'-DUSE_CLANG_COMPLETER',
 # THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
 # language to use when compiling headers. So it will guess. Badly. So C++
 # headers will be compiled as C headers. You don't want that so ALWAYS specify
@@ -59,12 +58,8 @@ flags = [
 # For a C project, you would set this to 'c' instead of 'c++'.
 '-x',
 'c',
-'-isystem',
-# This path will only work on OS X, but extra paths that don't exist are not
-# harmful
-'/System/Library/Frameworks/Python.framework/Headers',
 '-I',
-'.',
+'.'
 ]
 
 
