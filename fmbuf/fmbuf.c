f05ef32ae2f420ed660383f0ec38a3fbb7062e78
final <hyzwowtools@gmail.com>
2014-07-16 18:02:09 +0000

    bug fix and refactor fmbuf

--- fmbuf/fmbuf.c
+++ fmbuf/fmbuf.c
@@ -1,52 +1,57 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+
 #include "common/compiler.h"
 #include "fmbuf.h"
 
+// mbuf, contain the two pointer: the header and tailer, let's see the basic
+// use cases:
+// 1. the mbuf is empty: when the header == tailer
+// 2. the mbuf is full: when the next location of tailer == header
+
 #define MBUF_START(pbuf)    ( (char*)(pbuf->buf) )
-#define MBUF_END(pbuf)      ( pbuf->buf + pbuf->size - 1 )
+#define MBUF_END(pbuf)      ( pbuf->buf + pbuf->size )
 #define MBUF_HEAD(pbuf)     ( pbuf->head )
 #define MBUF_TAIL(pbuf)     ( pbuf->tail )
 #define MBUF_SIZE(pbuf)     ( pbuf->size )
-#define MBUF_USED(pbuf)     ( MBUF_HEAD(pbuf) <= MBUF_TAIL(pbuf) ? MBUF_TAIL(pbuf) - MBUF_HEAD(pbuf) : MBUF_SIZE(pbuf) + MBUF_TAIL(pbuf) - MBUF_HEAD(pbuf) )
-#define MBUF_FREE(pbuf)     ( MBUF_HEAD(pbuf) <= MBUF_TAIL(pbuf) ? MBUF_SIZE(pbuf) + MBUF_HEAD(pbuf) - MBUF_TAIL(pbuf) : MBUF_HEAD(pbuf) - MBUF_TAIL(pbuf) )
-#define MBUF_FIX_ADDR(size) ( size + ((size & 3) > 0)*(4 - (size & 3))  )        //fix the addr 
+#define MBUF_USED(pbuf) \
+    ( MBUF_HEAD(pbuf) <= MBUF_TAIL(pbuf) \
+      ? MBUF_TAIL(pbuf) - MBUF_HEAD(pbuf) \
+      : MBUF_SIZE(pbuf) - (MBUF_HEAD(pbuf) - MBUF_TAIL(pbuf)) + 1 )
+
+#define MBUF_FREE(pbuf) \
+    ( MBUF_HEAD(pbuf) <= MBUF_TAIL(pbuf) \
+      ? MBUF_SIZE(pbuf) - (MBUF_TAIL(pbuf) - MBUF_HEAD(pbuf)) \
+      : MBUF_HEAD(pbuf) - MBUF_TAIL(pbuf) - 1 )
+
+#define MBUF_COPY(dst, src, sz) \
+    if (sz > 0 && dst && src) { \
+        memcpy(dst, src, sz); \
+    }
 
 struct _mbuf {
     size_t size;
     char*  head;
     char*  tail;
-    char   buf[0];
+    char   buf[1];
 };
 
 fmbuf*    fmbuf_create(size_t size)
 {
-    if ( size > 0 ) {
-        fmbuf* pmbuf = malloc(sizeof(fmbuf) + size);
+    fmbuf* pmbuf = malloc(sizeof(fmbuf) + size);
+    pmbuf->size = size;
+    pmbuf->head = pmbuf->tail = pmbuf->buf;
 
-        if ( !pmbuf ) return NULL;
-        pmbuf->size = size;
-        pmbuf->head = pmbuf->tail = pmbuf->buf;
-
-        return pmbuf;
-    }
-    return NULL;
+    return pmbuf;
 }
 
 fmbuf*    fmbuf_create1(size_t size, int fill)
 {
-    if ( size > 0 ) {
-        fmbuf* pmbuf = malloc(sizeof(fmbuf) + size);
+    fmbuf* pmbuf = fmbuf_create(size);
+    memset(pmbuf->buf, fill, size);
 
-        if ( !pmbuf ) return NULL;
-        pmbuf->size = size;
-        pmbuf->head = pmbuf->tail = pmbuf->buf;
-        memset(pmbuf->buf, fill, size);
-
-        return pmbuf;
-    }
-    return NULL;
+    return pmbuf;
 }
 
 void    fmbuf_delete(fmbuf* pbuf)
@@ -54,21 +59,21 @@ void    fmbuf_delete(fmbuf* pbuf)
     free(pbuf);
 }
 
-int     fmbuf_push( fmbuf* pbuf, const void* data, size_t size)
+int     fmbuf_push(fmbuf* pbuf, const void* data, size_t size)
 {
-    if ( pbuf && data && size > 0 && ((size_t)MBUF_FREE(pbuf) -1) >= size ) {
-        size_t tail_free = MBUF_END(pbuf) - MBUF_TAIL(pbuf) + 1;
+    if (data && size > 0 && (size_t)MBUF_FREE(pbuf) >= size) {
+        size_t tail_free = fmbuf_tail_free(pbuf);
 
-        if ( tail_free >= size ) {
-            memcpy(MBUF_TAIL(pbuf), (char*)data, size);
+        if (tail_free >= size) {
+            memcpy(MBUF_TAIL(pbuf), data, size);
             MBUF_TAIL(pbuf) += size;
 
-            if( MBUF_TAIL(pbuf) > MBUF_END(pbuf) )
+            if(MBUF_TAIL(pbuf) > MBUF_END(pbuf))
                 MBUF_TAIL(pbuf) = MBUF_START(pbuf);
         } else {
-            memcpy(MBUF_TAIL(pbuf), (char*)data, tail_free);
+            memcpy(MBUF_TAIL(pbuf), data, tail_free);
             size_t left = size - tail_free;
-            memcpy(MBUF_START(pbuf), (char*)data+tail_free, left);
+            memcpy(MBUF_START(pbuf), data + tail_free, left);
             MBUF_TAIL(pbuf) = MBUF_START(pbuf) + left;
         }
 
@@ -80,21 +85,21 @@ int     fmbuf_push( fmbuf* pbuf, const void* data, size_t size)
 
 //pop data from head
 //if head to end space is less than size then go on to pop from start
-int        fmbuf_pop( fmbuf* pbuf, void* data, size_t size)
+int        fmbuf_pop(fmbuf* pbuf, void* data, size_t size)
 {
-    if ( pbuf && data && size > 0 && size <= (size_t)MBUF_USED(pbuf) ) {
+    if (size > 0 && size <= (size_t)MBUF_USED(pbuf)) {
         size_t tail_use = MBUF_END(pbuf) - MBUF_HEAD(pbuf) + 1;
 
-        if ( tail_use >= size ) {
-            memcpy((char*)data, MBUF_HEAD(pbuf), size);
+        if (tail_use >= size) {
+            MBUF_COPY(data, MBUF_HEAD(pbuf), size);
             MBUF_HEAD(pbuf) += size;
 
-            if ( MBUF_HEAD(pbuf) > MBUF_END(pbuf) )
+            if (MBUF_HEAD(pbuf) > MBUF_END(pbuf))
                 MBUF_HEAD(pbuf) = MBUF_START(pbuf);
         } else {
-            memcpy((char*)data, MBUF_HEAD(pbuf), tail_use);
+            MBUF_COPY(data, MBUF_HEAD(pbuf), tail_use);
             size_t left = size - tail_use;
-            memcpy((char*)data+tail_use, MBUF_START(pbuf), left);
+            MBUF_COPY(data + tail_use, MBUF_START(pbuf), left);
             MBUF_HEAD(pbuf) = MBUF_START(pbuf) + left;
         }
 
@@ -107,19 +112,19 @@ int        fmbuf_pop( fmbuf* pbuf, void* data, size_t size)
 // ensure you use the return value , that's safe
 void*    fmbuf_vpop(fmbuf* pbuf, void* data, size_t size)
 {
-    if ( pbuf && data && size > 0 && size <= (size_t)MBUF_USED(pbuf) ) {
+    if (data && size > 0 && size <= (size_t)MBUF_USED(pbuf)) {
         size_t tail_use = MBUF_END(pbuf) - MBUF_HEAD(pbuf) + 1;
 
-        if ( tail_use >= size ) {
+        if (tail_use >= size) {
             data = MBUF_HEAD(pbuf);
             MBUF_HEAD(pbuf) += size;
 
-            if( MBUF_HEAD(pbuf) > MBUF_END(pbuf) )
+            if(MBUF_HEAD(pbuf) > MBUF_END(pbuf))
                 MBUF_HEAD(pbuf) = MBUF_START(pbuf);
         } else {
             memcpy((char*)data, MBUF_HEAD(pbuf), tail_use);
             size_t left = size - tail_use;
-            memcpy((char*)data+tail_use, MBUF_START(pbuf), left);
+            memcpy((char*)data + tail_use, MBUF_START(pbuf), left);
             MBUF_HEAD(pbuf) = MBUF_START(pbuf) + left;
         }
 
@@ -129,35 +134,17 @@ void*    fmbuf_vpop(fmbuf* pbuf, void* data, size_t size)
     return NULL;        //pop failed
 }
 
-// only use to move the head ptr forward tail for sometime optimizating performance
-void    fmbuf_head_move(fmbuf* pbuf, size_t size)
+void*   fmbuf_rawget(fmbuf* pbuf, void* data, size_t size)
 {
-    if ( pbuf && size > 0 && size <= (size_t)MBUF_USED(pbuf) ) {
+    if (data && size > 0 && size <= (size_t)MBUF_USED(pbuf)) {
         size_t tail_use = MBUF_END(pbuf) - MBUF_HEAD(pbuf) + 1;
 
-        if ( tail_use >= size ) {
-            MBUF_HEAD(pbuf) += size;
-
-            if( unlikely(MBUF_HEAD(pbuf) > MBUF_END(pbuf)) )
-                MBUF_HEAD(pbuf) = MBUF_START(pbuf);
-        } else {
-            size_t left = size - tail_use;
-            MBUF_HEAD(pbuf) = MBUF_START(pbuf) + left;
-        }
-    }
-}
-
-void*   fmbuf_getraw(fmbuf* pbuf, void* data, size_t size)
-{
-    if ( pbuf && data && size > 0 && size <= (size_t)MBUF_USED(pbuf) ) {
-        size_t tail_use = MBUF_END(pbuf) - MBUF_HEAD(pbuf) + 1;
-
-        if ( tail_use >= size ) {
+        if (tail_use >= size) {
             data = MBUF_HEAD(pbuf);
         } else {
-            memcpy((char*)data, MBUF_HEAD(pbuf), tail_use);
+            memcpy(data, MBUF_HEAD(pbuf), tail_use);
             size_t left = size - tail_use;
-            memcpy((char*)data+tail_use, MBUF_START(pbuf), left);
+            memcpy(data + tail_use, MBUF_START(pbuf), left);
         }
 
         return data;    //get sucess
@@ -168,10 +155,10 @@ void*   fmbuf_getraw(fmbuf* pbuf, void* data, size_t size)
 
 void*   fmbuf_alloc(fmbuf* pbuf, size_t size)
 {
-    if ( fmbuf_tail_free(pbuf) < size )
+    if (fmbuf_tail_free(pbuf) < size)
         fmbuf_rewind(pbuf);
 
-    if ( fmbuf_tail_free(pbuf) >= size ) {
+    if (fmbuf_tail_free(pbuf) >= size) {
         char* ptr = MBUF_TAIL(pbuf);
         MBUF_TAIL(pbuf) += size;
 
@@ -183,15 +170,14 @@ void*   fmbuf_alloc(fmbuf* pbuf, size_t size)
 
 void    fmbuf_rewind(fmbuf* pbuf)
 {
-    int head_free = fmbuf_head_free(pbuf);
-
+    size_t head_free = fmbuf_head_free(pbuf);
     if (head_free == 0)
         return;
 
     memmove(MBUF_START(pbuf), MBUF_HEAD(pbuf), MBUF_USED(pbuf));
 
-    fmbuf_head_seek(pbuf, -head_free);
-    fmbuf_tail_seek(pbuf, -head_free);
+    fmbuf_head_seek(pbuf, head_free, FMBUF_SEEK_LEFT);
+    fmbuf_tail_seek(pbuf, head_free, FMBUF_SEEK_LEFT);
 }
 
 void    fmbuf_clear(fmbuf* pbuf)
@@ -199,14 +185,22 @@ void    fmbuf_clear(fmbuf* pbuf)
     MBUF_HEAD(pbuf) = MBUF_TAIL(pbuf) = MBUF_START(pbuf);
 }
 
-void    fmbuf_head_seek(fmbuf* pbuf, int offset)
+void    fmbuf_head_seek(fmbuf* pbuf, size_t offset, int direction)
 {
-    MBUF_HEAD(pbuf) += offset;
+    if (direction == FMBUF_SEEK_LEFT) {
+        MBUF_HEAD(pbuf) -= offset;
+    } else {
+        MBUF_HEAD(pbuf) += offset;
+    }
 }
 
-void    fmbuf_tail_seek(fmbuf* pbuf, int offset)
+void    fmbuf_tail_seek(fmbuf* pbuf, size_t offset, int direction)
 {
-    MBUF_TAIL(pbuf) += offset;
+    if (direction == FMBUF_SEEK_LEFT) {
+        MBUF_TAIL(pbuf) -= offset;
+    } else {
+        MBUF_TAIL(pbuf) += offset;
+    }
 }
 
 size_t  fmbuf_used(fmbuf* pbuf)
@@ -214,19 +208,14 @@ size_t  fmbuf_used(fmbuf* pbuf)
     return MBUF_USED(pbuf);
 }
 
-size_t  fmbuf_total_free(fmbuf* pbuf)
+size_t  fmbuf_free(fmbuf* pbuf)
 {
     return MBUF_FREE(pbuf);
 }
 
-size_t  fmbuf_free(fmbuf* pbuf)
-{
-    return fmbuf_total_free(pbuf) - 1;
-}
-
 size_t  fmbuf_tail_free(fmbuf* pbuf)
 {
-    return MBUF_END(pbuf) - MBUF_TAIL(pbuf) + 1;
+    return MBUF_END(pbuf) - MBUF_TAIL(pbuf);
 }
 
 size_t  fmbuf_head_free(fmbuf* pbuf)
@@ -234,35 +223,112 @@ size_t  fmbuf_head_free(fmbuf* pbuf)
     return MBUF_HEAD(pbuf) - MBUF_START(pbuf);
 }
 
-fmbuf*   fmbuf_realloc(fmbuf* pbuf, size_t size)
+static
+fmbuf*  _increase_buf(fmbuf* pbuf, size_t size)
 {
-    size_t total_size = MBUF_SIZE(pbuf);
-    if ( total_size == size )
-        return pbuf;
+    size_t head_offset = MBUF_HEAD(pbuf) - MBUF_START(pbuf);
+    size_t tail_offset = MBUF_TAIL(pbuf) - MBUF_START(pbuf);
 
-    size_t tail_pos = MBUF_TAIL(pbuf) - MBUF_START(pbuf);
-    if ( tail_pos > size ) {
-        fmbuf_rewind(pbuf);
-        tail_pos = MBUF_TAIL(pbuf) - MBUF_START(pbuf);
-        size = tail_pos > size ? tail_pos : size;
+    if (MBUF_HEAD(pbuf) <= MBUF_TAIL(pbuf)) {
+        fmbuf* new_buf = realloc(pbuf, sizeof(fmbuf) + size);
+        MBUF_HEAD(new_buf) = MBUF_START(new_buf) + head_offset;
+        MBUF_TAIL(new_buf) = MBUF_START(new_buf) + tail_offset;
+        MBUF_SIZE(new_buf) = size;
+        return new_buf;
+    } else {
+        // 1. calculate used buffer size for:
+        //   * start to tailer
+        //   * header to end
+        // 2. move the min part
+
+        // 1.
+        size_t increased_sz = size - MBUF_SIZE(pbuf);
+        size_t left_used = tail_offset;
+        size_t right_used = MBUF_USED(pbuf) - left_used;
+        fmbuf* new_buf = realloc(pbuf, sizeof(fmbuf) + size);
+        MBUF_HEAD(new_buf) = MBUF_START(new_buf) + head_offset;
+        MBUF_TAIL(new_buf) = MBUF_START(new_buf) + tail_offset;
+
+        // 2.
+        if (left_used <= right_used && left_used <= increased_sz) {
+            void* dst = MBUF_END(new_buf) + 1;
+            memmove(dst, MBUF_START(new_buf), left_used);
+            MBUF_TAIL(new_buf) = dst + left_used;
+            MBUF_SIZE(new_buf) = size;
+
+            if(MBUF_TAIL(new_buf) > MBUF_END(new_buf))
+                MBUF_TAIL(new_buf) = MBUF_START(new_buf);
+        } else {
+            void* dst = MBUF_HEAD(new_buf) + increased_sz;
+            memmove(dst, MBUF_HEAD(new_buf), right_used);
+            MBUF_HEAD(new_buf) += increased_sz;
+        }
+
+        return new_buf;
     }
-
-    size_t head_pos = MBUF_HEAD(pbuf) - MBUF_START(pbuf);
-    fmbuf* new_buf = realloc(pbuf, sizeof(fmbuf) + size);
-
-    MBUF_SIZE(new_buf) = size;
-    MBUF_HEAD(new_buf) = MBUF_START(new_buf) + head_pos;
-    MBUF_TAIL(new_buf) = MBUF_START(new_buf) + tail_pos;
-
-    return new_buf;
 }
 
-void*   fmbuf_get_head(fmbuf* pbuf)
+static
+fmbuf* _decrease_buf(fmbuf* pbuf, size_t size)
+{
+    size_t tail_offset = MBUF_TAIL(pbuf) - MBUF_START(pbuf);
+
+    if (MBUF_HEAD(pbuf) <= MBUF_TAIL(pbuf)) {
+        if (tail_offset > size) {
+            fmbuf_rewind(pbuf);
+            tail_offset = MBUF_TAIL(pbuf) - MBUF_START(pbuf);
+            size = tail_offset > size ? tail_offset : size;
+        }
+
+        size_t head_offset = MBUF_HEAD(pbuf) - MBUF_START(pbuf);
+        fmbuf* new_buf = realloc(pbuf, sizeof(fmbuf) + size);
+        MBUF_HEAD(new_buf) = MBUF_START(new_buf) + head_offset;
+        MBUF_TAIL(new_buf) = MBUF_START(new_buf) + tail_offset;
+        MBUF_SIZE(new_buf) = size;
+
+        return new_buf;
+    } else {
+        size_t left_used = tail_offset;
+        size_t right_used = MBUF_USED(pbuf) - left_used;
+
+        size_t decreased_sz = MBUF_SIZE(pbuf) - size;
+        size_t free_sz = MBUF_FREE(pbuf);
+        size_t move_sz = free_sz >= decreased_sz ? decreased_sz : free_sz;
+        size_t actual_sz = MBUF_SIZE(pbuf) - move_sz;
+        size_t head_offset = MBUF_HEAD(pbuf) - MBUF_START(pbuf);
+
+        void* dst = MBUF_HEAD(pbuf) - move_sz;
+        memmove(dst, MBUF_HEAD(pbuf), right_used);
+        MBUF_HEAD(pbuf) -= move_sz;
+        head_offset = MBUF_HEAD(pbuf) - MBUF_START(pbuf);
+
+        fmbuf* new_buf = realloc(pbuf, sizeof(fmbuf) + actual_sz);
+        MBUF_HEAD(new_buf) = MBUF_START(new_buf) + head_offset;
+        MBUF_TAIL(new_buf) = MBUF_START(new_buf) + tail_offset;
+        MBUF_SIZE(new_buf) = actual_sz;
+
+        return new_buf;
+    }
+}
+
+fmbuf*  fmbuf_realloc(fmbuf* pbuf, size_t size)
+{
+    size_t total_size = MBUF_SIZE(pbuf);
+    if (total_size == size) {
+        return pbuf;
+    } else if (total_size < size) {
+        return _increase_buf(pbuf, size);
+    } else { // total_size > size
+        return _decrease_buf(pbuf, size);
+    }
+}
+
+void*   fmbuf_head(fmbuf* pbuf)
 {
     return MBUF_HEAD(pbuf);
 }
 
-void*   fmbuf_get_tail(fmbuf* pbuf)
+void*   fmbuf_tail(fmbuf* pbuf)
 {
     return MBUF_TAIL(pbuf);
 }
--- fmbuf/fmbuf.h
+++ fmbuf/fmbuf.h
@@ -1,5 +1,6 @@
 /**
- *  Light Weight Memory Buffer, which can be used for Ring-Buffer or A Simple Array
+ *  Light Weight Memory Buffer, which can be used for Ring-Buffer or A Simple
+ *  Array
  */
 
 #ifndef MBUF_H_FINAL
@@ -9,42 +10,128 @@
 extern "C" {
 #endif
 
+#include <stddef.h>
+
+#define FMBUF_SEEK_LEFT  0
+#define FMBUF_SEEK_RIGHT 1
+
 typedef struct _mbuf fmbuf;
 
 fmbuf*  fmbuf_create(size_t size);
-fmbuf*  fmbuf_create1(size_t size, int fill); // create a fmbuf populate by 'fill'
-void    fmbuf_delete(fmbuf* pbuf);
 
-// public interface 
-size_t  fmbuf_size(fmbuf* pbuf);
-size_t  fmbuf_used(fmbuf* pbuf);
-void    fmbuf_clear(fmbuf* pbuf);
-void*   fmbuf_get_head(fmbuf* pbuf);
-void*   fmbuf_get_tail(fmbuf* pbuf);
+// create a fmbuf which be populated by 'fill'
+fmbuf*  fmbuf_create1(size_t size, int fill);
+void    fmbuf_delete(fmbuf* mbuf);
+
+// public interface
+size_t  fmbuf_size(fmbuf* mbuf);
+size_t  fmbuf_free(fmbuf* mbuf);
+size_t  fmbuf_used(fmbuf* mbuf);
+void    fmbuf_clear(fmbuf* mbuf);
+void*   fmbuf_head(fmbuf* mbuf);
+void*   fmbuf_tail(fmbuf* mbuf);
+fmbuf*  fmbuf_realloc(fmbuf* mbuf, size_t size);
 
 // ----------------As a Ring Buff------------------------
-//push data into mbuf : if tail free space not enough that continue write from start
-//return 0/1 : 0-> sucess 1->fail
-int     fmbuf_push(fmbuf* pbuf, const void* data, size_t size);
-int     fmbuf_pop(fmbuf* pbuf, void* data, size_t size);
 
-void*   fmbuf_vpop(fmbuf* pbuf, void* data, size_t size);
-void*   fmbuf_getraw(fmbuf* pbuf, void* data, size_t size);
-void    fmbuf_head_move(fmbuf* pbuf, size_t size);
-size_t  fmbuf_free(fmbuf* pbuf);
+// push data into mbuf
+//
+// @param pbuf  mbuf
+// @param data  the data user want to push to mbuf
+// @param size  the size of data
+//
+// @return
+//   - **0**: success
+//   - **1**: failure
+int     fmbuf_push(fmbuf* mbuf, const void* data, size_t size);
+
+// pop data from mbuf
+//
+// @param pbuf  mbuf
+// @param buf   the data will be copied to the user *buffer*
+// @param size  the size of the user buf
+//
+// @note if the buf is NULL, the data will still be poped up
+//
+// @return
+//   - **0**: success
+//   - **1**: failure: the mbuf size < user expected size
+int     fmbuf_pop(fmbuf* mbuf, void* buf, size_t size);
+
+// pop data from mbuf, but actually almost 99% pop action won't involve the
+// data copy, it just return the data pointer of the real data
+//
+// @note user *MUST ALWAYS* use the return pointer as the data pointer
+//
+// @param pbuf  mbuf
+// @param buf   the data will be copied to the user *buffer* (99% it won't copy)
+// @param size  the size of the user buf
+//
+// @return      the real data pointer
+void*   fmbuf_vpop(fmbuf* mbuf, void* buf, size_t size);
+
+// get the data pointer of raw data, and it won't consume the data, so user
+// still can pop the data later
+//
+// @note user *MUST ALWAYS* use the return pointer as the data pointer
+//
+// @param pbuf  mbuf
+// @param buf   the data will be copied to the user *buffer* (99% it won't copy)
+// @param size  the size of the user buf
+//
+// @return      the real data pointer
+void*   fmbuf_rawget(fmbuf* mbuf, void* buf, size_t size);
 
 //-----------------As a Array-----------------------------
-//alloc a buf from mbuf
-//return NULL : alloc failed
-size_t  fmbuf_head_free(fmbuf* pbuf);
-size_t  fmbuf_tail_free(fmbuf* pbuf);
-size_t  fmbuf_total_free(fmbuf* pbuf);
 
-void*   fmbuf_alloc(fmbuf* pbuf, size_t size);
-void    fmbuf_head_seek(fmbuf* pbuf, int offset);
-void    fmbuf_tail_seek(fmbuf* pbuf, int offset);
-void    fmbuf_rewind(fmbuf* pbuf);
-fmbuf*  fmbuf_realloc(fmbuf* pbuf, size_t size);
+// Allocate a specific size of buffer from the mbuf
+//
+// @param pbuf  mbuf
+// @param size  the size user want to allocate
+//
+// @return
+//   - the begin location of the buffer
+//   - NULL when there is no enough space
+void*   fmbuf_alloc(fmbuf* mbuf, size_t size);
+
+// Move the header pointer of the mbuf
+//
+// @param pbuf      mbuf
+// @param bytes     the distance of user want to move,
+// @param direction - FMBUF_SEEK_LEFT: move left the header
+//                  - FMBUF_SEEK_LEFT: move right the header
+//
+// @return void
+void    fmbuf_head_seek(fmbuf* mbuf, size_t offset, int direction);
+
+// Move the tailer pointer of the mbuf
+//
+// @param pbuf      mbuf
+// @param offset    the distance of user want to move,
+// @param direction - FMBUF_SEEK_LEFT: move left the tailer
+//                  - FMBUF_SEEK_LEFT: move right the tailer
+//
+// @return void
+void    fmbuf_tail_seek(fmbuf* mbuf, size_t offset, int direction);
+
+// Reset the header pointer to the begin of the mbuf, then move the data and
+// tailer pointer as well
+//
+// @param mbuf
+// @return void
+void    fmbuf_rewind(fmbuf* mbuf);
+
+// return the how many free space before the header pointer
+//
+// @param mbuf
+// @return void
+size_t  fmbuf_head_free(fmbuf* mbuf);
+
+// return the how many free space after the tailer pointer
+//
+// @param mbuf
+// @return void
+size_t  fmbuf_tail_free(fmbuf* mbuf);
 
 #ifdef __cplusplus
 }
