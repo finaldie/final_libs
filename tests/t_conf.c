ece4959b2965200eaf947523d1743deb69a79325
Yuzhang Hu <hyzwowtools@gmail.com>
2015-01-12 16:25:18 +0000

    fix and pass fconf UT

--- tests/t_conf.c
+++ tests/t_conf.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "ftu_inc.h"
+#include "flibs/ftu_inc.h"
+#include "flibs/fconf.h"
 #include "inc.h"
-#include "fread_conf.h"
 
-#define TEST_CONF1 "./config/t1.conf"
-#define TEST_CONF2 "./config/t2.conf"
-#define TEST_CONF3 "./config/t3.conf"
+#define TEST_CONF1 "tests/config/t1.conf"
+#define TEST_CONF2 "tests/config/t2.conf"
+#define TEST_CONF3 "tests/config/t3.conf"
 
 void config_lookup(const char* key, const char* value)
 {
@@ -18,9 +18,9 @@ void config_lookup(const char* key, const char* value)
 void test_conf()
 {
     printf("loading config 1\n");
-    FTU_ASSERT_EQUAL_INT(0, fload_config(TEST_CONF1, config_lookup));
+    FTU_ASSERT_EQUAL_INT(0, fconf_load(TEST_CONF1, config_lookup));
     printf("loading config 2\n");
-    FTU_ASSERT_EQUAL_INT(1, fload_config(TEST_CONF2, config_lookup));
+    FTU_ASSERT_EQUAL_INT(1, fconf_load(TEST_CONF2, config_lookup));
     printf("loading config 3\n");
-    FTU_ASSERT_EQUAL_INT(1, fload_config(TEST_CONF3, config_lookup));
+    FTU_ASSERT_EQUAL_INT(1, fconf_load(TEST_CONF3, config_lookup));
 }
